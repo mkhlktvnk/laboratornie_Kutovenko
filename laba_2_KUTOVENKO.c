f02c2e707fd2dc047e253c939e03ea94adf28992
Mikhail Kutovenko <70900496+mkhlktvnk@users.noreply.github.com>
2020-09-09 18:09:03 +0300

    Add files via upload

---
+++ laba_2_KUTOVENKO.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+#include <string.h>
+#include <ctype.h>
+// посчитать кол-во слов в строке
+// задание 12 
+#include <stdio.h>
+
+int main() {
+    char str[255];
+    int i, col_vo, f;
+    printf_s("Enter the string\n");
+    gets_s(str);
+    col_vo = 0;
+    f = 0;
+    for (i = 0; str[i] != '\0'; i++)
+        if (str[i] != ' ' && f == 0 && isalpha(str[i])) {
+            col_vo += 1;
+            f = 1;
+        }
+        else
+            if (str[i] == ' ') f = 0;
+    printf("col-vo slov: %d\n", col_vo);
+    return 0;
+}
